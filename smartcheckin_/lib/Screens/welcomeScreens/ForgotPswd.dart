9a4159014845e7b6d75525d5e7aff66284005381
HBK <rainheritier@gmail.com>
2020-09-23 10:11:41 +0200

    splited the 4 pages in different classes (files) still need to fix some stuff here and there

---
+++ smartcheckin_/lib/Screens/welcomeScreens/ForgotPswd.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'SignUpScreen.dart';
+import 'HomeScreen.dart';
+
+class ForgotPswd extends StatefulWidget {
+  @override
+  _ForgotPswdState createState() => _ForgotPswdState();
+}
+
+class _ForgotPswdState extends State<ForgotPswd> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
